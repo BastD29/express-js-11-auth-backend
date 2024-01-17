d36d28cb8ad384f11533d5a8b92652c88e30a152
BastD29 <bastiendaniel9@gmail.com>
2024-01-17 14:52:11 +0100

    added POST method to cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(
   cors({
     origin: "https://auth-t5a0.onrender.com",
-    methods: "GET",
+    methods: ["GET", "POST"],
     allowedHeaders: "Content-Type,Authorization",
   })
 );
